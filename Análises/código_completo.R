d18916dbcf128b3642eb2d7863f7a244f54953bb
André Costa <147108697+andresrcosta@users.noreply.github.com>
2023-12-09 17:03:35 -0300

    Update código_completo.R

--- Análises/código_completo.R
+++ Análises/código_completo.R
@@ -141,6 +141,13 @@ vendas_final <- vendas_secundario[, !(names(vendas_secundario) %in% c("X"))]
 #FINAL FORMATAÇÃO#
 ##################
 
+######################
+##### Importante ##### 
+######################
+
+# 1 - o banco de dados utilizado foi formatado no código formatacao.R
+# 2 - O tema dos gráficos está no começo do código completo do github
+
 ######################################################
 #### Analise 1 - Faturamento anual por categoria #####
 ######################################################
@@ -151,6 +158,12 @@ vendas_final <- vendas_secundario[, !(names(vendas_secundario) %in% c("X"))]
 # não entram no faturamento #
 
 vendas_analise_1 <-vendas_sem_devolucao
+
+# Listando as categorias
+
+tipos_categorias <- unique(vendas_analise_1$Categoria)
+
+print(tipos_categorias)
 
 # Retirando os NAS#
 
@@ -213,6 +226,153 @@ quadro_resumo_analise_1 <- faturamento_mensal %>%
              `Máximo ` = round(max(Faturamento),2)) %>% t() %>% as.data.frame()
 
 xtable :: xtable(quadro_resumo_analise_1)
+
+# Teste
+
+# Testando a normalidade dos dados para implementar a ANOVA
+
+categorias_analise_1 <- unique(vendas_analise_1$Categoria)
+
+for (marca in Categorias_analise_1) {
+  subset_data <- vendas_analise_2$Preço[vendas_analise_2$Marca == marca]
+  result_ad_test <- ad.test(subset_data)
+  print(paste("Teste Anderson-Darling para", marca))
+  print(result_ad_test)
+}
+
+# Critério de normalidade satisfeito
+
+#Anova para a média de preços entre as marcas
+
+modelo_anova <- aov(Preço ~ Marca, data = vendas_analise_2)
+resultado_anova <- summary(modelo_anova)
+
+# Exibir os resultados da ANOVA
+print(resultado_anova)
+
+# Analisando as devoluções de produtos com defeito
+
+# Usando o dataset completo
+
+vendas_analise_1_parte_2 <-vendas_final
+
+# Retirando NAS
+
+vendas_analise_1_parte_2 <- subset(vendas_analise_1_parte_2, complete.cases(Categoria,`Motivo de devolução 2`,`Mês da compra`))
+
+# Tipos de devoluções
+
+tipos_devolucao <- unique(vendas_analise_1_parte_2$`Motivo de devolução 2`)
+
+print(tipos_devolucao)
+
+# Agrupando as devoluções
+
+devolucoes_mensais_defeito <- vendas_analise_1_parte_2 %>%
+  filter(`Motivo de devolução 2` == "Produto com defeito") %>%
+  group_by(`Mês da compra`, Categoria) %>%
+  summarise(Total_Devolucoes = n()) %>%
+  ungroup()
+
+xtable :: xtable(devolucoes_mensais_defeito)
+
+# Gráfico de linhas multivariado para o total de devoluções por mês para cada motivo
+
+ggplot(devolucoes_mensais_defeito) +
+  aes(x = `Mês da compra`, y = Total_Devolucoes, group = Categoria, color = Categoria) +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
+  labs(x = "Mês", y = "Total de devoluções", color = "Categoria") +
+  scale_y_continuous(limits = c(0, max(devolucoes_mensais_defeito$Total_Devolucoes) + 5), breaks = seq(0, max(devolucoes_mensais_defeito$Total_Devolucoes) + 5, by = 5)) +
+  theme_estat()
+ggsave("devolucoes_mensais_defeito_final.pdf", width = 158, height = 93, units = "mm")
+
+# Somando todos os preço
+
+soma_precos_defeito_por_categoria <- vendas_analise_1_parte_2 %>%
+  filter(`Motivo de devolução 2` == "Produto com defeito") %>%
+  group_by(Categoria) %>%
+  summarise(Total_Preco = sum(Preço, na.rm = TRUE)) %>%
+  ungroup()
+
+# Exibindo a soma dos preços por categoria
+
+print(soma_precos_defeito_por_categoria)
+
+xtable :: xtable(soma_precos_defeito_por_categoria)
+
+# Avaliando cada marca individualmente
+
+# Retirando NAS
+
+vendas_analise_1_parte_3_1 <- subset(vendas_analise_1, complete.cases(Categoria,Marca))
+
+# Grafico de setories com todas as marcas
+
+contagem <- vendas_analise_1_parte_3_1 %>%
+  group_by(Categoria,Marca) %>%
+  summarise (Freq = n()) %>%
+  mutate(Prop = round(100*(Freq/sum(Freq)),2)) %>%
+  arrange(desc(Marca)) %>%
+  mutate(posicao = cumsum(Prop) - 0.5*Prop)
+
+contagem %>%
+  ggplot (aes(x = factor(""), y = Prop , fill = (Marca))) +
+  geom_bar(width = 1, stat = "identity") +
+  facet_wrap(~ Categoria) +
+  coord_polar("y", start = 0) +
+  geom_text(
+    aes(x = 2, y = posicao , label = paste0(Prop , "%")),
+    color = "black") +
+  theme_void () +
+  theme(legend.position = "top") +
+  scale_fill_manual(values = cores_estat , name = 'Marca')
+ggsave("setor_final_1.pdf", width = 180, height = 93, units = "mm")
+
+# Gráficos de linhas multivariados individuais para cada marca
+
+# Função para criar os gráficos
+
+gerar_grafico <- function(data, nome_arquivo) {
+  grafico <- ggplot(data) +
+    aes(x = `Mês da compra`, y = Faturamento, group = Marca, color = Marca) +
+    geom_line(size = 1) +
+    geom_point(size = 2) +
+    labs(x = "Mês", y = "Faturamento", color = "Marca") +
+    scale_y_continuous(limits = c(0, max(data$Faturamento) + 100), breaks = seq(0, max(data$Faturamento) + 100, by = 100)) +
+    theme_minimal()
+  
+  ggsave(nome_arquivo, plot = grafico, width = 158, height = 93, units = "mm")
+}
+
+# Gráficos de linhas multivariados para diferentes colunas: marca, cor e tamanho
+
+# Listando das colunas de interesse
+
+colunas_interesse <- c("Marca", "Cor", "Tamanho")
+
+for (coluna in colunas_interesse) {
+  # Filtrando dados sem valores ausentes para a coluna atual e Categoria
+  dados_filtrados <- subset(vendas_analise_1, complete.cases(Categoria, {{coluna}}))
+  
+  # Agrupando o faturamento anual por coluna atual e Categoria
+  categorias <- c("Women's Fashion", "Men's Fashion", "Kids' Fashion")
+  
+  for (cat in categorias) {
+    dataset_name <- paste("faturamento_anual_", gsub("'", "", gsub(" ", "_", cat)), "_", gsub("'", "", gsub(" ", "_", coluna)), sep = "")
+    assign(dataset_name, calcular_faturamento(dados_filtrados, cat, coluna))
+  }
+  
+  # Criando lista de dataframes e nomes de arquivos para a coluna atual
+  nomes_arquivos <- paste("faturamento_", gsub("'", "", gsub(" ", "_", coluna)), "_", gsub("'", "", gsub(" ", "_", coluna)), "_final.pdf", sep = "")
+  nomes_arquivos <- rep(nomes_arquivos, length(categorias))
+  dataframes <- mget(grep(paste("^faturamento_anual_", gsub("'", "", gsub(" ", "_", coluna)), "_", sep = ""), ls(), value = TRUE))
+  
+  # Iterando para criar os gráficos para a coluna atual
+  for (i in seq_along(dataframes)) {
+    gerar_grafico(dataframes[[i]], nomes_arquivos[i])
+  }
+}
 
 #################################################
 ####Analise 2 - Variação do preço por marca #####

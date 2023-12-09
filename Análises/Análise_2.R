f8a18bfd06d16c807c75e0ad2a186e3267344cbb
André Costa <147108697+andresrcosta@users.noreply.github.com>
2023-12-09 16:56:12 -0300

    Update Análise_2.R

--- Análises/Análise_2.R
+++ Análises/Análise_2.R
@@ -68,3 +68,59 @@ resultado_anova <- summary(modelo_anova)
 
 # Exibir os resultados da ANOVA
 print(resultado_anova)
+
+# Lista de marcas de interesse
+marcas_interesse <- c("Adidas", "H&M", "Gucci", "Zara", "Nike")
+
+# Criando uma lista vazia para armazenar os dataframes filtrados
+lista_vendas_por_marca <- list()
+
+# Loop para criar um dataframe para cada marca
+for (marca in marcas_interesse) {
+  nome_dataset <- paste("vendas_", tolower(gsub("&", "", gsub("'", "", gsub(" ", "_", marca)))), sep = "")
+  dataset_filtrado <- vendas_analise_2 %>%
+    filter(Marca == marca)
+  
+  # Armazenando o dataframe na lista usando o nome da marca como identificação
+  lista_vendas_por_marca[[nome_dataset]] <- dataset_filtrado
+}
+
+# Graficos e testes para cada marca
+
+# Lista de marcas de interesse
+
+marcas_interesse <- c("Nike", "H&M", "Adidas", "Gucci")
+
+# Lista de categorias de interesse
+
+categorias_interesse <- c("Cor", "Categoria", "Tamanho")
+
+# Função para criar boxplots e realizar testes ANOVA
+
+gerar_boxplot_anova <- function(data, var_x, var_y, nome_arquivo) {
+  ggplot(data) +
+    aes_string(x = var_x, y = var_y) +
+    geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+    stat_summary(fun = "mean", geom = "point", shape = 23, size = 3, fill = "white") +
+    labs(x = var_x, y = var_y) +
+    theme_minimal()
+  
+  ggsave(nome_arquivo, width = 158, height = 93, units = "mm")
+  
+  modelo_anova <- aov(as.formula(paste(var_y, "~", var_x)), data = data)
+  resultado_anova <- summary(modelo_anova)
+  print(resultado_anova)
+}
+
+# Loop para criar boxplots e realizar testes ANOVA para cada marca e categoria
+
+for (marca in marcas_interesse) {
+  for (categoria in categorias_interesse) {
+    nome_dataset <- paste("vendas_", tolower(gsub("&", "", gsub("'", "", gsub(" ", "_", marca)))), sep = "")
+    dataset_filtrado <- subset(vendas_analise_2, Marca == marca & complete.cases({{categoria}}, Preço))
+    
+    nome_arquivo <- paste("box_", tolower(gsub("&", "", gsub("'", "", gsub(" ", "_", marca)))), "_", categoria, ".pdf", sep = "")
+    
+    gerar_boxplot_anova(dataset_filtrado, categoria, "Preço", nome_arquivo)
+  }
+}

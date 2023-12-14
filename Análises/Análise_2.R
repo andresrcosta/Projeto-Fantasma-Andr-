924047dba7f7304e5d7dce2b09140669e83de4db
André Costa <147108697+andresrcosta@users.noreply.github.com>
2023-12-14 01:31:46 -0300

    Update Análise_2.R

--- Análises/Análise_2.R
+++ Análises/Análise_2.R
@@ -105,7 +105,7 @@ gerar_boxplot_anova <- function(data, var_x, var_y, nome_arquivo) {
     geom_boxplot(fill = c("#A11D21"), width = 0.5) +
     stat_summary(fun = "mean", geom = "point", shape = 23, size = 3, fill = "white") +
     labs(x = var_x, y = var_y) +
-    theme_minimal()
+    theme_estat()
   
   ggsave(nome_arquivo, width = 158, height = 93, units = "mm")
   
